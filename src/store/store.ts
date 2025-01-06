8731bbd9d81c0d73572a0400e4d3bc47f5c23c66
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2025-01-06 17:56:33 +0500

    🚀 Deploy:

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
-export interface Task {
+interface Task {
   id: string;
   name: string;
   frequency: "daily" | "weekly";
